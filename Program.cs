31368d8c000fd95bb37d987acc76fdd5a65c9e73
Salvador <sarevalotamayo@student.rtc.edu>
2024-03-08 22:48:52 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Arrays_3
+{
+    internal class Program
+    {
+        static double[] numbers = { 5.2, 7.9, 10.0, 4, 9.7 };
+        private static double[] number;
+        static void MultiplyByTwo(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= 2;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before:");
+            DisplayArray(numbers);
+
+            MultiplyByTwo(number);
+
+            Console.WriteLine("After:");
+            DisplayArray(numbers);
+        }
+
+        static void DisplayArray(double[] array)
+        {
+            Console.Write("{ ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.WriteLine(" }");
+        }
+    }
+}
